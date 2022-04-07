f934a22a9ae65515cf20151a1ee27378b60473e3
Tanapol Kampoh <74783058+munggon@users.noreply.github.com>
2022-04-08 03:30:21 +0700

    Delete NewsSubscriber.java

--- NewsSubscriber.java
+++
@@ -1,8 +0,0 @@
-package edu.parinya.softarchdesign.behavioral;
-
-import java.util.concurrent.Flow.Subscriber;
-import java.util.concurrent.Flow.Subscription;
-
-public class NewsSubscriber implements Subscriber<News> {
-	// You need to implement this class
-}

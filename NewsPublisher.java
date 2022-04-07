3aa428cad07862021f395795065cda9c1922ae0e
Tanapol Kampoh <74783058+munggon@users.noreply.github.com>
2022-04-08 03:30:27 +0700

    Delete NewsPublisher.java

--- NewsPublisher.java
+++
@@ -1,8 +0,0 @@
-package edu.parinya.softarchdesign.behavioral;
-
-import java.util.concurrent.Flow.Publisher;
-import java.util.concurrent.Flow.Subscriber;
-
-public class NewsPublisher implements Publisher<News> {
-	// You need to implement this class
-}
